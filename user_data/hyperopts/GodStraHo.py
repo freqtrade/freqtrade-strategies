48e9fcd062ea4cac5d071b07139f622d0452308d
Masoud Azizi <mablue92@gmail.com>
2021-04-28 21:36:54 +0430

    Speed Up + No need to AgeFilter in config now

--- user_data/hyperopts/GodStraHo.py
+++ user_data/hyperopts/GodStraHo.py
@@ -3,7 +3,7 @@
 # github: https://github.com/mablue/
 # IMPORTANT: INSTALL TA BEFOUR RUN:
 # :~$ pip install ta
-# freqtrade hyperopt --hyperopt GodStraHo --hyperopt-loss SharpeHyperOptLossDaily --spaces all --strategy GodStra --config config.json -e 100
+# freqtrade hyperopt --hyperopt GodStraHo --hyperopt-loss SharpeHyperOptLossDaily --gene all --strategy GodStra --config config.json -e 100
 
 # --- Do not remove these libs ---
 from functools import reduce
@@ -22,33 +22,51 @@ from freqtrade.optimize.hyperopt_interface import IHyperOpt
 from ta import add_all_ta_features
 from ta.utils import dropna
 import freqtrade.vendor.qtpylib.indicators as qtpylib
+from random import randint
 # this is your trading strategy DNA Size
 # you can change it and see the results...
 DNA_SIZE = 1
+SELL_DNA_SIZE = 1
+tplist = [7, 14]
+GodGeneIndicators = ['ACOS', 'AD', 'ADD', 'ADOSC', 'ADX', 'ADXR', 'APO',
+                     'AROON0', 'AROON1', 'AROONOSC', 'ASIN', 'ATAN', 'ATR', 'AVGPRICE',
+                     'BBANDS0', 'BBANDS1', 'BBANDS2', 'BETA',
+                     'BOP', 'CCI', 'CDL2CROWS', 'CDL3BLACKCROWS', 'CDL3INSIDE', 'CDL3LINESTRIKE',
+                     'CDL3OUTSIDE', 'CDL3STARSINSOUTH', 'CDL3WHITESOLDIERS', 'CDLABANDONEDBABY',
+                     'CDLADVANCEBLOCK', 'CDLBELTHOLD', 'CDLBREAKAWAY', 'CDLCLOSINGMARUBOZU',
+                     'CDLCONCEALBABYSWALL', 'CDLCOUNTERATTACK', 'CDLDARKCLOUDCOVER', 'CDLDOJI',
+                     'CDLDOJISTAR', 'CDLDRAGONFLYDOJI', 'CDLENGULFING', 'CDLEVENINGDOJISTAR',
+                     'CDLEVENINGSTAR', 'CDLGAPSIDESIDEWHITE', 'CDLGRAVESTONEDOJI', 'CDLHAMMER',
+                     'CDLHANGINGMAN', 'CDLHARAMI', 'CDLHARAMICROSS', 'CDLHIGHWAVE', 'CDLHIKKAKE',
+                     'CDLHIKKAKEMOD', 'CDLHOMINGPIGEON', 'CDLIDENTICAL3CROWS', 'CDLINNECK',
+                     'CDLINVERTEDHAMMER', 'CDLKICKING', 'CDLKICKINGBYLENGTH', 'CDLLADDERBOTTOM',
+                     'CDLLONGLEGGEDDOJI', 'CDLLONGLINE', 'CDLMARUBOZU', 'CDLMATCHINGLOW',
+                     'CDLMATHOLD', 'CDLMORNINGDOJISTAR', 'CDLMORNINGSTAR', 'CDLONNECK',
+                     'CDLPIERCING', 'CDLRICKSHAWMAN', 'CDLRISEFALL3METHODS', 'CDLSEPARATINGLINES',
+                     'CDLSHOOTINGSTAR', 'CDLSHORTLINE', 'CDLSPINNINGTOP', 'CDLSTALLEDPATTERN',
+                     'CDLSTICKSANDWICH', 'CDLTAKURI', 'CDLTASUKIGAP', 'CDLTHRUSTING', 'CDLTRISTAR',
+                     'CDLUNIQUE3RIVER', 'CDLUPSIDEGAP2CROWS', 'CDLXSIDEGAP3METHODS', 'CEIL', 'CMO',
+                     'CORREL', 'COS', 'COSH', 'DEMA', 'DIV', 'DX', 'EMA', 'EXP', 'FLOOR',
+                     'HT_DCPERIOD', 'HT_DCPHASE',
+                     'HT_PHASOR0', 'HT_PHASOR1', 'HT_SINE0', 'HT_SINE1', 'HT_TRENDLINE',
+                     'HT_TRENDMODE', 'KAMA', 'LINEARREG', 'LINEARREG_ANGLE', 'LINEARREG_INTERCEPT',
+                     'LINEARREG_SLOPE', 'LN', 'LOG10', 'MA',
+                     'MACD0', 'MACD1', 'MACD2', 'MACDEXT0', 'MACDEXT1', 'MACDEXT2',
+                     'MACDFIX0', 'MACDFIX1', 'MACDFIX2', 'MAMA0', 'MAMA1',
+                     'MAX', 'MAXINDEX', 'MEDPRICE', 'MFI', 'MIDPOINT', 'MIDPRICE',
+                     'MIN', 'MININDEX', 'MINMAX0', 'MINMAX1', 'MINMAXINDEX0', 'MINMAXINDEX1',
+                     'MINUS_DI', 'MINUS_DM', 'MOM',
+                     'MULT', 'NATR', 'OBV', 'PLUS_DI', 'PLUS_DM', 'PPO', 'ROC', 'ROCP', 'ROCR',
+                     'ROCR100', 'RSI', 'SAR', 'SAREXT', 'SIN', 'SINH', 'SMA', 'SQRT', 'STDDEV',
+                     'STOCH0', 'STOCH1', 'STOCHF0', 'STOCHF1', 'STOCHRSI0', 'STOCHRSI1',
+                     'SUB', 'SUM', 'T3', 'TAN', 'TANH', 'TEMA',
+                     'TRANGE', 'TRIMA', 'TRIX', 'TSF', 'TYPPRICE', 'ULTOSC', 'VAR', 'WCLPRICE',
+                     'WILLR', 'WMA']
+GodGenes = list()
 
-
-GodGenes = ["open", "high", "low", "close", "volume", "volume_adi", "volume_obv",
-            "volume_cmf", "volume_fi", "volume_mfi", "volume_em", "volume_sma_em", "volume_vpt",
-            "volume_nvi", "volume_vwap", "volatility_atr", "volatility_bbm", "volatility_bbh",
-            "volatility_bbl", "volatility_bbw", "volatility_bbp", "volatility_bbhi",
-            "volatility_bbli", "volatility_kcc", "volatility_kch", "volatility_kcl",
-            "volatility_kcw", "volatility_kcp", "volatility_kchi", "volatility_kcli",
-            "volatility_dcl", "volatility_dch", "volatility_dcm", "volatility_dcw",
-            "volatility_dcp", "volatility_ui", "trend_macd", "trend_macd_signal",
-            "trend_macd_diff", "trend_sma_fast", "trend_sma_slow", "trend_ema_fast",
-            "trend_ema_slow", "trend_adx", "trend_adx_pos", "trend_adx_neg", "trend_vortex_ind_pos",
-            "trend_vortex_ind_neg", "trend_vortex_ind_diff", "trend_trix",
-            "trend_mass_index", "trend_cci", "trend_dpo", "trend_kst",
-            "trend_kst_sig", "trend_kst_diff", "trend_ichimoku_conv",
-            "trend_ichimoku_base", "trend_ichimoku_a", "trend_ichimoku_b",
-            "trend_visual_ichimoku_a", "trend_visual_ichimoku_b", "trend_aroon_up",
-            "trend_aroon_down", "trend_aroon_ind", "trend_psar_up", "trend_psar_down",
-            "trend_psar_up_indicator", "trend_psar_down_indicator", "trend_stc",
-            "momentum_rsi", "momentum_stoch_rsi", "momentum_stoch_rsi_k",
-            "momentum_stoch_rsi_d", "momentum_tsi", "momentum_uo", "momentum_stoch",
-            "momentum_stoch_signal", "momentum_wr", "momentum_ao", "momentum_kama",
-            "momentum_roc", "momentum_ppo", "momentum_ppo_signal", "momentum_ppo_hist",
-            "others_dr", "others_dlr", "others_cr"]
+for gene in GodGeneIndicators:
+    for tp in tplist:
+        GodGenes.append(f'{gene}-{tp}')
 
 
 class GodStraHo(IHyperOpt):
@@ -61,6 +79,8 @@ class GodStraHo(IHyperOpt):
         gene = list()
 
         for i in range(DNA_SIZE):
+            # g = randint(0, 5)
+            # GodGenes = GodGenesGroup[g]
             gene.append(Categorical(GodGenes, name=f'buy-indicator-{i}'))
             gene.append(Categorical(GodGenes, name=f'buy-cross-{i}'))
             gene.append(Integer(-1, 101, name=f'buy-int-{i}'))
@@ -132,7 +152,7 @@ class GodStraHo(IHyperOpt):
         """
         gene = list()
 
-        for i in range(DNA_SIZE):
+        for i in range(SELL_DNA_SIZE):
             gene.append(Categorical(GodGenes, name=f'sell-indicator-{i}'))
             gene.append(Categorical(GodGenes, name=f'sell-cross-{i}'))
             gene.append(Integer(-1, 101, name=f'sell-int-{i}'))
@@ -156,7 +176,7 @@ class GodStraHo(IHyperOpt):
             conditions = []
 
             # GUARDS AND TRENDS
-            for i in range(DNA_SIZE):
+            for i in range(SELL_DNA_SIZE):
 
                 OPR = params[f'sell-oper-{i}']
                 IND = params[f'sell-indicator-{i}']
