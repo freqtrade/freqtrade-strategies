72d47b4fd136181cba69cb3ca4086c1d429951a7
Masoud Azizi <mablue92@gmail.com>
2021-05-05 10:14:51 +0430

    Indicators Normalized!!! FFFFFFFFFFFFFFFfffffffffffffffuuuu*****

--- user_data/hyperopts/GodStraHo.py
+++ user_data/hyperopts/GodStraHo.py
@@ -16,56 +16,58 @@ from skopt.space import Categorical, Dimension, Integer, Real  # noqa
 from freqtrade.optimize.hyperopt_interface import IHyperOpt
 
 # --------------------------------
-# Add your lib to import here
-# import talib.abstract as ta  # noqa
-from ta import add_all_ta_features
-from ta.utils import dropna
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from random import randint
 # this is your trading strategy DNA Size
 # you can change it and see the results...
 DNA_SIZE = 1
 SELL_DNA_SIZE = 1
-tplist = [7, 14]
-GodGeneIndicators = ['ACOS', 'AD', 'ADD', 'ADOSC', 'ADX', 'ADXR', 'APO',
-                     'AROON0', 'AROON1', 'AROONOSC', 'ASIN', 'ATAN', 'ATR', 'AVGPRICE',
-                     'BBANDS0', 'BBANDS1', 'BBANDS2', 'BETA',
-                     'BOP', 'CCI', 'CDL2CROWS', 'CDL3BLACKCROWS', 'CDL3INSIDE', 'CDL3LINESTRIKE',
-                     'CDL3OUTSIDE', 'CDL3STARSINSOUTH', 'CDL3WHITESOLDIERS', 'CDLABANDONEDBABY',
-                     'CDLADVANCEBLOCK', 'CDLBELTHOLD', 'CDLBREAKAWAY', 'CDLCLOSINGMARUBOZU',
-                     'CDLCONCEALBABYSWALL', 'CDLCOUNTERATTACK', 'CDLDARKCLOUDCOVER', 'CDLDOJI',
-                     'CDLDOJISTAR', 'CDLDRAGONFLYDOJI', 'CDLENGULFING', 'CDLEVENINGDOJISTAR',
-                     'CDLEVENINGSTAR', 'CDLGAPSIDESIDEWHITE', 'CDLGRAVESTONEDOJI', 'CDLHAMMER',
-                     'CDLHANGINGMAN', 'CDLHARAMI', 'CDLHARAMICROSS', 'CDLHIGHWAVE', 'CDLHIKKAKE',
-                     'CDLHIKKAKEMOD', 'CDLHOMINGPIGEON', 'CDLIDENTICAL3CROWS', 'CDLINNECK',
-                     'CDLINVERTEDHAMMER', 'CDLKICKING', 'CDLKICKINGBYLENGTH', 'CDLLADDERBOTTOM',
-                     'CDLLONGLEGGEDDOJI', 'CDLLONGLINE', 'CDLMARUBOZU', 'CDLMATCHINGLOW',
-                     'CDLMATHOLD', 'CDLMORNINGDOJISTAR', 'CDLMORNINGSTAR', 'CDLONNECK',
-                     'CDLPIERCING', 'CDLRICKSHAWMAN', 'CDLRISEFALL3METHODS', 'CDLSEPARATINGLINES',
-                     'CDLSHOOTINGSTAR', 'CDLSHORTLINE', 'CDLSPINNINGTOP', 'CDLSTALLEDPATTERN',
-                     'CDLSTICKSANDWICH', 'CDLTAKURI', 'CDLTASUKIGAP', 'CDLTHRUSTING', 'CDLTRISTAR',
-                     'CDLUNIQUE3RIVER', 'CDLUPSIDEGAP2CROWS', 'CDLXSIDEGAP3METHODS', 'CEIL', 'CMO',
-                     'CORREL', 'COS', 'COSH', 'DEMA', 'DIV', 'DX', 'EMA', 'EXP', 'FLOOR',
-                     'HT_DCPERIOD', 'HT_DCPHASE',
-                     'HT_PHASOR0', 'HT_PHASOR1', 'HT_SINE0', 'HT_SINE1', 'HT_TRENDLINE',
-                     'HT_TRENDMODE', 'KAMA', 'LINEARREG', 'LINEARREG_ANGLE', 'LINEARREG_INTERCEPT',
-                     'LINEARREG_SLOPE', 'LN', 'LOG10', 'MA',
-                     'MACD0', 'MACD1', 'MACD2', 'MACDEXT0', 'MACDEXT1', 'MACDEXT2',
-                     'MACDFIX0', 'MACDFIX1', 'MACDFIX2', 'MAMA0', 'MAMA1',
-                     'MAX', 'MAXINDEX', 'MEDPRICE', 'MFI', 'MIDPOINT', 'MIDPRICE',
-                     'MIN', 'MININDEX', 'MINMAX0', 'MINMAX1', 'MINMAXINDEX0', 'MINMAXINDEX1',
-                     'MINUS_DI', 'MINUS_DM', 'MOM',
-                     'MULT', 'NATR', 'OBV', 'PLUS_DI', 'PLUS_DM', 'PPO', 'ROC', 'ROCP', 'ROCR',
-                     'ROCR100', 'RSI', 'SAR', 'SAREXT', 'SIN', 'SINH', 'SMA', 'SQRT', 'STDDEV',
-                     'STOCH0', 'STOCH1', 'STOCHF0', 'STOCHF1', 'STOCHRSI0', 'STOCHRSI1',
-                     'SUB', 'SUM', 'T3', 'TAN', 'TANH', 'TEMA',
-                     'TRANGE', 'TRIMA', 'TRIX', 'TSF', 'TYPPRICE', 'ULTOSC', 'VAR', 'WCLPRICE',
-                     'WILLR', 'WMA']
+tplist = [5, 10, 50, 100]
+GodGeneIndicators = [
+    'ACOS', 'AD', 'ADD', 'ADOSC', 'ADX', 'ADXR', 'APO',
+    'AROON0', 'AROON1', 'AROONOSC', 'ASIN', 'ATAN', 'ATR', 'AVGPRICE',
+    'BBANDS0', 'BBANDS1', 'BBANDS2', 'BETA',
+    'BOP', 'CCI', 'CDL2CROWS', 'CDL3BLACKCROWS', 'CDL3INSIDE', 'CDL3LINESTRIKE',
+    'CDL3OUTSIDE', 'CDL3STARSINSOUTH', 'CDL3WHITESOLDIERS', 'CDLABANDONEDBABY',
+    'CDLADVANCEBLOCK', 'CDLBELTHOLD', 'CDLBREAKAWAY', 'CDLCLOSINGMARUBOZU',
+    'CDLCONCEALBABYSWALL', 'CDLCOUNTERATTACK', 'CDLDARKCLOUDCOVER', 'CDLDOJI',
+    'CDLDOJISTAR', 'CDLDRAGONFLYDOJI', 'CDLENGULFING', 'CDLEVENINGDOJISTAR',
+    'CDLEVENINGSTAR', 'CDLGAPSIDESIDEWHITE', 'CDLGRAVESTONEDOJI', 'CDLHAMMER',
+    'CDLHANGINGMAN', 'CDLHARAMI', 'CDLHARAMICROSS', 'CDLHIGHWAVE', 'CDLHIKKAKE',
+    'CDLHIKKAKEMOD', 'CDLHOMINGPIGEON', 'CDLIDENTICAL3CROWS', 'CDLINNECK',
+    'CDLINVERTEDHAMMER', 'CDLKICKING', 'CDLKICKINGBYLENGTH', 'CDLLADDERBOTTOM',
+    'CDLLONGLEGGEDDOJI', 'CDLLONGLINE', 'CDLMARUBOZU', 'CDLMATCHINGLOW',
+    'CDLMATHOLD', 'CDLMORNINGDOJISTAR', 'CDLMORNINGSTAR', 'CDLONNECK',
+    'CDLPIERCING', 'CDLRICKSHAWMAN', 'CDLRISEFALL3METHODS', 'CDLSEPARATINGLINES',
+    'CDLSHOOTINGSTAR', 'CDLSHORTLINE', 'CDLSPINNINGTOP', 'CDLSTALLEDPATTERN',
+    'CDLSTICKSANDWICH', 'CDLTAKURI', 'CDLTASUKIGAP', 'CDLTHRUSTING', 'CDLTRISTAR',
+    'CDLUNIQUE3RIVER', 'CDLUPSIDEGAP2CROWS', 'CDLXSIDEGAP3METHODS', 'CEIL', 'CMO',
+    'CORREL', 'COS', 'COSH', 'DEMA', 'DIV', 'DX', 'EMA', 'EXP', 'FLOOR',
+    'HT_DCPERIOD', 'HT_DCPHASE',
+    'HT_PHASOR0', 'HT_PHASOR1', 'HT_SINE0', 'HT_SINE1', 'HT_TRENDLINE',
+    'HT_TRENDMODE', 'KAMA', 'LINEARREG', 'LINEARREG_ANGLE', 'LINEARREG_INTERCEPT',
+    'LINEARREG_SLOPE', 'LN', 'LOG10', 'MA',
+    'MACD0', 'MACD1', 'MACD2', 'MACDEXT0', 'MACDEXT1', 'MACDEXT2',
+    'MACDFIX0', 'MACDFIX1', 'MACDFIX2', 'MAMA0', 'MAMA1',
+    'MAX', 'MAXINDEX', 'MEDPRICE', 'MFI', 'MIDPOINT', 'MIDPRICE',
+    'MIN', 'MININDEX', 'MINMAX0', 'MINMAX1', 'MINMAXINDEX0', 'MINMAXINDEX1',
+    'MINUS_DI', 'MINUS_DM', 'MOM',
+    'MULT', 'NATR', 'OBV', 'PLUS_DI', 'PLUS_DM', 'PPO', 'ROC', 'ROCP', 'ROCR',
+    'ROCR100', 'RSI', 'SAR', 'SAREXT', 'SIN', 'SINH', 'SMA', 'SQRT', 'STDDEV',
+    'STOCH0', 'STOCH1', 'STOCHF0', 'STOCHF1', 'STOCHRSI0', 'STOCHRSI1',
+    'SUB', 'SUM', 'T3', 'TAN', 'TANH', 'TEMA',
+    'TRANGE', 'TRIMA', 'TRIX', 'TSF', 'TYPPRICE', 'ULTOSC', 'VAR', 'WCLPRICE',
+    'WILLR', 'WMA'
+]
+
+
 GodGenes = list()
 
 for gene in GodGeneIndicators:
     for tp in tplist:
         GodGenes.append(f'{gene}-{tp}')
+
+# GodGenes.extend(['open', 'high', 'low', 'close', 'volume'])
 
 
 class GodStraHo(IHyperOpt):
@@ -78,17 +80,23 @@ class GodStraHo(IHyperOpt):
         gene = list()
 
         for i in range(DNA_SIZE):
-            # g = randint(0, 5)
-            # GodGenes = GodGenesGroup[g]
             gene.append(Categorical(GodGenes, name=f'buy-indicator-{i}'))
             gene.append(Categorical(GodGenes, name=f'buy-cross-{i}'))
-            gene.append(Integer(-1, 101, name=f'buy-int-{i}'))
-            gene.append(Real(-1.1, 1.1, name=f'buy-real-{i}'))
+            gene.append(Real(-1, 1, name=f'buy-real-{i}'))
             # Operations
             # CA: Crossed Above, CB: Crossed Below,
-            # I: Integer, R: Real, D: Disabled
-            gene.append(Categorical(["D", ">", "<", "=", "CA", "CB",
-                                     ">I", "=I", "<I", ">R", "=R", "<R"], name=f'buy-oper-{i}'))
+            # R: Real, D: Disabled
+            gene.append(Categorical([
+                "D",
+                ">",
+                "<",
+                "=",
+                "CA",
+                "CB",
+                ">R",
+                "=R",
+                "<R",
+            ], name=f'buy-oper-{i}'))
         return gene
 
     @staticmethod
@@ -107,7 +115,6 @@ class GodStraHo(IHyperOpt):
                 OPR = params[f'buy-oper-{i}']
                 IND = params[f'buy-indicator-{i}']
                 CRS = params[f'buy-cross-{i}']
-                INT = params[f'buy-int-{i}']
                 REAL = params[f'buy-real-{i}']
                 DFIND = dataframe[IND]
                 DFCRS = dataframe[CRS]
@@ -122,12 +129,6 @@ class GodStraHo(IHyperOpt):
                     conditions.append(qtpylib.crossed_above(DFIND, DFCRS))
                 elif OPR == "CB":
                     conditions.append(qtpylib.crossed_below(DFIND, DFCRS))
-                elif OPR == ">I":
-                    conditions.append(DFIND > INT)
-                elif OPR == "=I":
-                    conditions.append(DFIND == INT)
-                elif OPR == "<I":
-                    conditions.append(DFIND < INT)
                 elif OPR == ">R":
                     conditions.append(DFIND > REAL)
                 elif OPR == "=R":
@@ -154,13 +155,12 @@ class GodStraHo(IHyperOpt):
         for i in range(SELL_DNA_SIZE):
             gene.append(Categorical(GodGenes, name=f'sell-indicator-{i}'))
             gene.append(Categorical(GodGenes, name=f'sell-cross-{i}'))
-            gene.append(Integer(-1, 101, name=f'sell-int-{i}'))
-            gene.append(Real(-0.01, 1.01, name=f'sell-real-{i}'))
+            gene.append(Real(-1, 1, name=f'sell-real-{i}'))
             # Operations
             # CA: Crossed Above, CB: Crossed Below,
-            # I: Integer, R: Real, D: Disabled
+            # R: Real, D: Disabled
             gene.append(Categorical(["D", ">", "<", "=", "CA", "CB",
-                                     ">I", "=I", "<I", ">R", "=R", "<R"], name=f'sell-oper-{i}'))
+                                     ">R", "=R", "<R"], name=f'sell-oper-{i}'))
         return gene
 
     @ staticmethod
@@ -180,7 +180,6 @@ class GodStraHo(IHyperOpt):
                 OPR = params[f'sell-oper-{i}']
                 IND = params[f'sell-indicator-{i}']
                 CRS = params[f'sell-cross-{i}']
-                INT = params[f'sell-int-{i}']
                 REAL = params[f'sell-real-{i}']
                 DFIND = dataframe[IND]
                 DFCRS = dataframe[CRS]
@@ -195,12 +194,6 @@ class GodStraHo(IHyperOpt):
                     conditions.append(qtpylib.crossed_above(DFIND, DFCRS))
                 elif OPR == "CB":
                     conditions.append(qtpylib.crossed_below(DFIND, DFCRS))
-                elif OPR == ">I":
-                    conditions.append(DFIND > INT)
-                elif OPR == "=I":
-                    conditions.append(DFIND == INT)
-                elif OPR == "<I":
-                    conditions.append(DFIND < INT)
                 elif OPR == ">R":
                     conditions.append(DFIND > REAL)
                 elif OPR == "=R":
