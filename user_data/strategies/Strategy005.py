73c3c34b10a313e5844ba7690542c31edff7ac05
Matthias <xmatthias@outlook.com>
2021-08-03 20:38:53 +0200

    COnvert Strategy005 to Hyperoptable Strategy

--- user_data/strategies/Strategy005.py
+++ user_data/strategies/Strategy005.py
@@ -1,7 +1,7 @@
 
 # --- Do not remove these libs ---
-from freqtrade.strategy.interface import IStrategy
-from typing import Dict, List
+from freqtrade.strategy import IStrategy
+from freqtrade.strategy import CategoricalParameter, IntParameter
 from functools import reduce
 from pandas import DataFrame
 # --------------------------------
@@ -20,6 +20,7 @@ class Strategy005(IStrategy):
     How to use it?
     > python3 ./freqtrade/main.py -s Strategy005
     """
+    INTERFACE_VERSION = 2
 
     # Minimal ROI designed for the strategy.
     # This attribute will be overridden if the config file contains "minimal_roi"
@@ -58,6 +59,34 @@ class Strategy005(IStrategy):
         'stoploss': 'market',
         'stoploss_on_exchange': False
     }
+
+    buy_volumeAVG = IntParameter(low=50, high=300, default=70, space='buy', optimize=True)
+    buy_rsi = IntParameter(low=1, high=100, default=30, space='buy', optimize=True)
+    buy_fastd = IntParameter(low=1, high=100, default=30, space='buy', optimize=True)
+    buy_fishRsiNorma = IntParameter(low=1, high=100, default=30, space='buy', optimize=True)
+
+    sell_rsi = IntParameter(low=1, high=100, default=70, space='sell', optimize=True)
+    sell_minusDI = IntParameter(low=1, high=100, default=50, space='sell', optimize=True)
+    sell_fishRsiNorma = IntParameter(low=1, high=100, default=50, space='sell', optimize=True)
+    sell_trigger = CategoricalParameter(["rsi-macd-minusdi", "sar-fisherRsi"],
+                                        default=30, space='sell', optimize=True)
+
+    # Buy hyperspace params:
+    buy_params = {
+        "buy_fastd": 1,
+        "buy_fishRsiNorma": 5,
+        "buy_rsi": 26,
+        "buy_volumeAVG": 150,
+    }
+
+    # Sell hyperspace params:
+    sell_params = {
+        "sell_fishRsiNorma": 30,
+        "sell_minusDI": 4,
+        "sell_rsi": 74,
+        "sell_trigger": "rsi-macd-minusdi",
+    }
+
 
     def informative_pairs(self):
         """
@@ -124,13 +153,12 @@ class Strategy005(IStrategy):
             # Prod
             (
                 (dataframe['close'] > 0.00000200) &
-                (dataframe['volume'] > dataframe['volume'].rolling(200).mean() * 4) &
+                (dataframe['volume'] > dataframe['volume'].rolling(self.buy_volumeAVG.value).mean() * 4) &
                 (dataframe['close'] < dataframe['sma']) &
                 (dataframe['fastd'] > dataframe['fastk']) &
-                (dataframe['rsi'] > 0) &
-                (dataframe['fastd'] > 0) &
-                # (dataframe['fisher_rsi'] < -0.94)
-                (dataframe['fisher_rsi_norma'] < 38.900000000000006)
+                (dataframe['rsi'] > self.buy_rsi.value) &
+                (dataframe['fastd'] > self.buy_fastd.value) &
+                (dataframe['fisher_rsi_norma'] < self.buy_fishRsiNorma.value)
             ),
             'buy'] = 1
 
@@ -142,17 +170,17 @@ class Strategy005(IStrategy):
         :param dataframe: DataFrame
         :return: DataFrame with buy column
         """
-        dataframe.loc[
-            # Prod
-            (
-                (qtpylib.crossed_above(dataframe['rsi'], 50)) &
-                (dataframe['macd'] < 0) &
-                (dataframe['minus_di'] > 0)
-            ) |
-            (
-                (dataframe['sar'] > dataframe['close']) &
-                (dataframe['fisher_rsi'] > 0.3)
-            ),
 
-            'sell'] = 1
+        conditions = []
+        if self.sell_trigger.value == 'rsi-macd-minusdi':
+            conditions.append(qtpylib.crossed_above(dataframe['rsi'], self.sell_rsi.value))
+            conditions.append(dataframe['macd'] < 0)
+            conditions.append(dataframe['minus_di'] > self.sell_minusDI.value)
+        if self.sell_trigger.value == 'sar-fisherRsi':
+            conditions.append(dataframe['sar'] > dataframe['close'])
+            conditions.append(dataframe['fisher_rsi'] > self.sell_fishRsiNorma.value)
+
+        if conditions:
+            dataframe.loc[reduce(lambda x, y: x & y, conditions), 'sell'] = 1
+
         return dataframe
