e4bf29db38dbd0570402d4c32c2f1da13d2155fd
Masoud Azizi <mablue92@gmail.com>
2021-08-26 00:32:37 +0000

    multima returned to the base | New Strategy Comming soon
    MultiMa big lines moved to Persia Stra Comming soon
    
    MultiMa big lines moved to Persia Stra Comming soon

--- user_data/strategies/MultiMa.py
+++ user_data/strategies/MultiMa.py
@@ -4,7 +4,8 @@
 # (First Hyperopt it.A hyperopt file is available)
 #
 # --- Do not remove these libs ---
-from freqtrade.strategy.hyper import IntParameter
+import pandas
+from freqtrade.strategy.hyper import CategoricalParameter, IntParameter
 from freqtrade.strategy.interface import IStrategy
 from pandas import DataFrame
 # --------------------------------
@@ -16,55 +17,95 @@ from functools import reduce
 
 
 class MultiMa(IStrategy):
+    # *    3/5000:    201 trades. 88/110/3 Wins/Draws/Losses. Avg profit   0.58%. Median profit   0.00%. Total profit  469.48458465 USDT (  46.95%). Avg duration 11:29:00 min. Objective: -469.48458
 
-    buy_ma_count = IntParameter(2, 10, default=10, space='buy')
-    buy_ma_gap = IntParameter(2, 10, default=2, space='buy')
-    buy_ma_shift = IntParameter(0, 10, default=0, space='buy')
-    # buy_ma_rolling = IntParameter(0, 10, default=0, space='buy')
 
-    sell_ma_count = IntParameter(2, 10, default=10, space='sell')
-    sell_ma_gap = IntParameter(2, 10, default=2, space='sell')
-    sell_ma_shift = IntParameter(0, 10, default=0, space='sell')
-    # sell_ma_rolling = IntParameter(0, 10, default=0, space='sell')
+    # Buy hyperspace params:
+    buy_params = {
+        "buy_ma_comp": "!=",
+        "buy_ma_count": 150,
+        "buy_ma_driver": "g",
+        "buy_ma_rolling": 80,
+        "buy_ma_shift": 90,
+    }
+
+    # Sell hyperspace params:
+    sell_params = {
+        "sell_ma_comp": ">",
+        "sell_ma_count": 150,
+        "sell_ma_driver": "s",
+        "sell_ma_rolling": 54,
+        "sell_ma_shift": 14,
+    }
 
     # ROI table:
     minimal_roi = {
-        "0": 0.30873,
-        "569": 0.16689,
-        "3211": 0.06473,
-        "7617": 0
+        "0": 0.119,
+        "34": 0.07,
+        "50": 0.022,
+        "110": 0
     }
 
     # Stoploss:
-    stoploss = -0.1
+    stoploss = -0.267
 
-    # Buy hypers
-    timeframe = '4h'
+    # Trailing stop:
+    trailing_stop = False  # value loaded from strategy
+    trailing_stop_positive = None  # value loaded from strategy
+    trailing_stop_positive_offset = 0.0  # value loaded from strategy
+    trailing_only_offset_is_reached = False  # value loaded from strategy
+    
+    # Timeframe
+    timeframe = '5m'
+
+    
+    timeframes = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200]
+    buy_ma_count = CategoricalParameter(timeframes, default=10, space='buy')
+    buy_ma_rolling = IntParameter(0, 100, default=2, space='buy')
+    buy_ma_shift = IntParameter(0, 100, default=0, space='buy')
+    buy_ma_driver = CategoricalParameter(['c', 'g', 's'], default='s', space='buy')
+    buy_ma_comp = CategoricalParameter(['<','>','!=','=='], default='>', space='buy')
+    
+    sell_ma_count = CategoricalParameter(timeframes, default=10, space='sell')
+    sell_ma_rolling = IntParameter(0, 100, default=2, space='sell')
+    sell_ma_shift = IntParameter(0, 100, default=0, space='sell')
+    sell_ma_driver = CategoricalParameter(['c', 'g', 's'], default='s', space='sell')
+    sell_ma_comp = CategoricalParameter(['<','>','!=','=='], default='<', space='sell')
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-       
-        # We will dinamicly generate the indicators
-        # cuz this method just run one time in hyperopts
-        # if you have static timeframes you can move first loop of buy and sell trends populators inside this method
+
+        for i in self.buy_ma_count.range:
+            dataframe[i] = ta.MA(dataframe, timeperiod=int(i))
+
+        for i in self.sell_ma_count.range:
+            dataframe[i] = ta.MA(dataframe, timeperiod=int(i))
 
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        
-        for i in self.buy_ma_count.range:
-            dataframe[f'buy-ma-{i+1}'] = ta.SMA(dataframe,
-                                              timeperiod=int((i+1) * self.buy_ma_gap.value))
-        
-        conditions = []
 
-        for i in self.buy_ma_count.range:
-            if i > 1:
-                shift = self.buy_ma_shift.value
-                for shift in self.buy_ma_shift.range:
-                    conditions.append(
-                        dataframe[f'buy-ma-{i}'].shift(shift) >
-                        dataframe[f'buy-ma-{i-1}'].shift(shift)
-                    )
+        conditions = []
+        if self.buy_ma_driver.value == 'c':
+            rng = self.buy_ma_count.range
+            for idx, count in enumerate(rng):
+                if idx-1 > 1:
+                    dataframe['A'] = dataframe[count].rolling(int(self.buy_ma_rolling.value)).sum().shift(self.buy_ma_shift.value)
+                    dataframe['B'] =  dataframe.iloc[:, idx - 1].rolling(int(self.buy_ma_rolling.value)).sum().shift(self.buy_ma_shift.value)
+                    conditions.append(dataframe.eval(f' A {self.buy_ma_comp.value} B '))
+        elif self.buy_ma_driver.value == 'g':
+            rng = self.buy_ma_rolling.range
+            for rolling in rng:
+                if rolling-1 >= 0:
+                    dataframe['A'] = dataframe[self.buy_ma_count.value].rolling(rolling).sum().shift(self.buy_ma_shift.value)
+                    dataframe['B'] =  dataframe[self.buy_ma_count.value].rolling(int(rolling-1)).sum().shift(self.buy_ma_shift.value)
+                    conditions.append(dataframe.eval(f' A {self.buy_ma_comp.value} B '))
+        elif self.buy_ma_driver.value == 's':
+            rng = self.buy_ma_shift.range
+            for shift in rng:
+                dataframe['A'] = dataframe[self.buy_ma_count.value].rolling(int(self.buy_ma_rolling.value)).sum().shift(shift)
+                dataframe['B'] =  dataframe[self.buy_ma_count.value].rolling(int(self.buy_ma_rolling.value)).sum().shift(shift-1)
+                conditions.append(dataframe.eval(f' A {self.buy_ma_comp.value} B '))
+
         if conditions:
             dataframe.loc[
                 reduce(lambda x, y: x & y, conditions),
@@ -73,20 +114,28 @@ class MultiMa(IStrategy):
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        for i in self.sell_ma_count.range:
-            dataframe[f'sell-ma-{i+1}'] = ta.SMA(dataframe,
-                                               timeperiod=int((i+1) * self.sell_ma_gap.value))
-
         conditions = []
+        if self.sell_ma_driver.value == 'c':
+            rng = self.sell_ma_count.range
+            for idx, count in enumerate(rng):
+                if idx-1 > 1:
+                    dataframe['A'] = dataframe[count].rolling(int(self.sell_ma_rolling.value)).sum().shift(self.sell_ma_shift.value)
+                    dataframe['B'] =  dataframe.iloc[:, idx - 1].rolling(int(self.sell_ma_rolling.value)).sum().shift(self.sell_ma_shift.value)
+                    conditions.append(dataframe.eval(f' A {self.sell_ma_comp.value} B '))
+        elif self.sell_ma_driver.value == 'g':
+            rng = self.sell_ma_rolling.range
+            for rolling in rng:
+                if rolling-1 >= 0:
+                    dataframe['A'] = dataframe[self.sell_ma_count.value].rolling(rolling).sum().shift(self.sell_ma_shift.value)
+                    dataframe['B'] =  dataframe[self.sell_ma_count.value].rolling(int(rolling-1)).sum().shift(self.sell_ma_shift.value)
+                    conditions.append(dataframe.eval(f' A {self.sell_ma_comp.value} B '))
+        elif self.sell_ma_driver.value == 's':
+            rng = self.sell_ma_shift.range
+            for shift in rng:
+                dataframe['A'] = dataframe[self.sell_ma_count.value].rolling(int(self.sell_ma_rolling.value)).sum().shift(shift)
+                dataframe['B'] =  dataframe[self.sell_ma_count.value].rolling(int(self.sell_ma_rolling.value)).sum().shift(shift-1)
+                conditions.append(dataframe.eval(f' A {self.sell_ma_comp.value} B '))
 
-        for i in self.sell_ma_count.range:
-            if i > 1:
-                shift = self.sell_ma_shift.value
-                for shift in self.sell_ma_shift.range:
-                    conditions.append(
-                        dataframe[f'sell-ma-{i}'].shift(shift) <
-                        dataframe[f'sell-ma-{i-1}'].shift(shift)
-                    )
         if conditions:
             dataframe.loc[
                 reduce(lambda x, y: x & y, conditions),
