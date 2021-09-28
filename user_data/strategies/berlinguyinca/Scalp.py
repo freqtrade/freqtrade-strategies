d14f1473816e6908a2ccb2b21ab61ae93866a26c
Redian Ibra <redian@users.noreply.github.com>
2021-09-28 17:11:37 +0200

    Update scalp.py
    Removing duplicated imports

--- user_data/strategies/berlinguyinca/Scalp.py
+++ user_data/strategies/berlinguyinca/Scalp.py
@@ -7,9 +7,6 @@ from pandas import DataFrame
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 # --------------------------------
-import talib.abstract as ta
-import freqtrade.vendor.qtpylib.indicators as qtpylib
-
 
 class Scalp(IStrategy):
     """
