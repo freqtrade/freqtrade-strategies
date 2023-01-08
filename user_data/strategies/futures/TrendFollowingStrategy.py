a197613a7ada99cd608dbf2f16c834cefe6a114c
Pedro Povoleri <pedro@povoleri.com>
2023-01-08 12:38:24 +0100

    Fix PR coments.

--- user_data/strategies/futures/TrendFollowingStrategy.py
+++ user_data/strategies/futures/TrendFollowingStrategy.py
@@ -7,6 +7,23 @@ import talib.abstract as ta
 from freqtrade.strategy.interface import IStrategy
 
 class TrendFollowingStrategy(IStrategy):
+
+    INTERFACE_VERSION: int = 3
+    # ROI table:
+    minimal_roi = {"0": 0.15, "30": 0.1, "60": 0.05}
+    # minimal_roi = {"0": 1}
+
+    # Stoploss:
+    stoploss = -0.265
+
+    # Trailing stop:
+    trailing_stop = True
+    trailing_stop_positive = 0.05
+    trailing_stop_positive_offset = 0.1
+    trailing_only_offset_is_reached = False
+
+    timeframe = "5m"
+
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # Calculate OBV
         dataframe['obv'] = ta.OBV(dataframe['close'], dataframe['volume'])
