ba8e59e794649025e2d21d1414b6bf5d7d81ebec
contributorINF113 <167693538+contributorINF113@users.noreply.github.com>
2024-04-21 16:14:30 +0200

    Update Bandtastic.py
    Updated populate_entry_trend and populate_exit_trend to follow new practices

--- user_data/strategies/Bandtastic.py
+++ user_data/strategies/Bandtastic.py
@@ -98,7 +98,7 @@ class Bandtastic(IStrategy):
 
         return dataframe
 
-    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         conditions = []
 
         # GUARDS
@@ -129,7 +129,7 @@ class Bandtastic(IStrategy):
 
         return dataframe
 
-    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         conditions = []
 
         # GUARDS
